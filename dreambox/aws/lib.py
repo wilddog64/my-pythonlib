5e3788a7bfc5242a20caaa5e2d5b288bf73ec1fb
chengkai liang <ckm.liang@gmail.com>
2015-02-05 11:03:15 -0800

    extract code into utils.py modules

--- dreambox/aws/lib.py
+++ dreambox/aws/lib.py
@@ -93,28 +93,6 @@ def aws_asgcmd(aws_profile='dreambox',
 
 
 
-# make_hash_of_hashes will make an array of hashes from a given list by these
-# steps,
-#
-#   1. create a pairwise list of tuples
-#   2. transfer turple into a list
-#   3. create a hash where key is the first element, and value is the reset
-#   4. append hash to a list
-#
-# the function takes the following parameters,
-#
-#   my_list: a valid python list
-#
-# return a list of hashes upon a succesful call
-def make_hash_of_hashes(my_list):
-    turple = zip(my_list[::2], my_list[1::2])
-    result = {}
-    for item in turple:
-        items = list(item)
-        result[items[0][0]] = items[1]
-    return result
-
-
 
 if __name__ == "__main__":
     from dreambox.aws.asg import *
