291bd1276f89966404f2f6a2d51f8c03186824f9
chengkai liang <ckm.liang@gmail.com>
2015-02-06 14:39:27 -0800

    initial ec2 class check-in

--- dreambox/aws/ec2.py
+++ dreambox/aws/ec2.py
@@ -1,3 +1,16 @@
-from dreambox.aws.core import *
+from dreambox.aws.core import aws_ec2cmd
 from funcy.seqs import *
 from itertools import chain
+
+
+class ec2:
+    profile=''
+    region=''
+    instances=None
+
+    def __init__(self, profile='dreambox', region='us-east-1'):
+       self.profile = profile
+       self.region = region
+
+    def describe_instances(self, **options):
+        self.instances = aws_ec2cmd(self.profile, self.region, **options)
