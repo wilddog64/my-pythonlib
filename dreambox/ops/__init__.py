93e0ada70db85fb67aa1db150a4b6cbe5fc2b49b
chengkai liang <ckm.liang@gmail.com>
2015-03-29 09:29:08 -0700

    export get_available_stack_from_all_regions

--- dreambox/ops/__init__.py
+++ dreambox/ops/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ['get_all_asgs',
            'get_all_play_asgs',
            'get_only_play_asgs',
-           'get_ec2_instances_hostnames_from_asg_groups']
+           'get_ec2_instances_hostnames_from_asg_groups',
+           'get_available_stack_from_all_regions']
