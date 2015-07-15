8f6498bad0fb3efc87186ad03012cf7472548c93
chengkai liang <ckm.liang@gmail.com>
2015-07-15 08:50:34 -0700

    add get_all_stacks_for_stage and test cases

--- dreambox/ops/cloudformation.py
+++ dreambox/ops/cloudformation.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 from dreambox.aws.core import aws_cfn_cmd
+from funcy.colls import select
+import dreambox.utils
 import re
+import sys
 
 
 def get_stack_names_from_all_regions(profile='',
@@ -35,3 +38,26 @@ number from 1 - 9)
     return region_stacks
 
 
+def get_all_stacks_for_stage(profile=None,
+                             region=None,
+                             filterby=None):
+    '''
+get_all_stacks_for_stage will return all the stacks exists for a given stage
+environment.  It takes the following parameters,
+
+profile - aws profile if one exist.  If it is not provided, the function will
+          try to use IAM profile if a given instance has one.
+region - which aws region do we want to work on
+filterby - limit result by providing a filter string
+    '''
+    stage_stacks = aws_cfn_cmd(aws_profile=profile,
+                               aws_region=region,
+                               cfn_subcmd='describe-stacks',
+                               query='Stacks[].StackName')
+
+    stacks_list = select(lambda x: filterby.lower() in x.lower(), stage_stacks)
+    return stacks_list
+
+if __name__ == '__main__':
+    stacks = get_all_stacks_for_stage(region='us-west-2', filterby='stage3')
+    dreambox.utils.print_structure(stacks)
