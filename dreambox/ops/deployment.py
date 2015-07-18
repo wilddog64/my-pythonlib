6e3a39091c74cd93d63b86af81d59aedb5864de3
chengkai liang <ckm.liang@gmail.com>
2015-07-18 09:35:04 -0700

    rename variable stage to partial_tag

--- dreambox/ops/deployment.py
+++ dreambox/ops/deployment.py
@@ -118,17 +118,17 @@ usage:
 def get_all_ec2_instances_from_tag(argv=None):
     '''
 usage:
-    ops get_all_ec2_instances_from_tag <stage>
+    ops get_all_ec2_instances_from_tag <partial_tag>
     '''
 
     from dreambox.ops.ec2 import get_ec2_hosts_for_stage
     arguments = docopt(get_all_ec2_instances_from_tag.__doc__, argv=argv)
-    stage = arguments['<stage>']
-    if stage.lower() == 'all':
-      stage=None
+    partial_tag = arguments['<partial_tag>']
+    if partial_tag.lower() == 'all':
+      partial_tag=None
 
-    print('query stage environment: {}'.format(stage))
-    stage_ec2_instances = get_ec2_hosts_for_stage(stage=stage)
+    print('query stage environment: {}'.format(partial_tag))
+    stage_ec2_instances = get_ec2_hosts_for_stage(stage=partial_tag)
     dreambox.utils.print_structure(stage_ec2_instances)
 
 
