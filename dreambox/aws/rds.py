9b0dc9907a250e3e38de6345b3f4fd6cd2ffa714
Chengkai Liang <chengkai.liang@dreambox.com>
2016-09-03 10:12:02 -0700

    move get_latest_rds_snapshot to rds_client.py

--- dreambox/aws/rds.py
+++ dreambox/aws/rds.py
@@ -26,40 +26,9 @@ caller. The function takes the following parameters:
 
     return db_snapshots
 
-def get_latest_rds_snapshot(profile='', region='us-east-1', env_prefix=''):
-    '''
-get_lastest_rds_snapshot is a function that will return a latest created snapshot
-base on the SnapshotCreateTime. This function takes the following parameters,
-
-* profile - an aws profile that provide credential information.  If this is
-            None, it will first look at a default profile setting at
-            ~/.aws/config. If it cannot find default profile, it will use node
-            IAM profile if there's one exist for a given node.
-* regions - a list of regions to search for a target environment. If this is
-            None, it will search for us-east-1 and us-west-2.
-* env_prefix - an environment prefix like stage1, ..., stage9
-    '''
-    db_snapshots = describe_rds_snapshots(profile=profile,
-                                          region=region,
-                                          env_prefix=env_prefix)
-    sort_key = 'SnapshotCreateTime'
-    decorated = []
-    try:
-        decorated = [(dateutil.parser.parse(dict_[sort_key]), dict_) for dict_ in db_snapshots]
-        decorated.sort(reverse=True)
-    except ValueError as ve:
-        print(ve)
-    return [dict_ for (key, dict_) in decorated][0]
-
-    return db_snapshots
-
 if __name__ == '__main__':
     print('--- testing describe_rds_snapshots ---')
     rds_db_snapshots = describe_rds_snapshots(env_prefix='playbackup')
     dreambox.utils.print_structure(rds_db_snapshots)
     print('--- testing describe_rds_snapshots ---')
     print('')
-    print('--- testing get_latest_rds_snapshot')
-    rds_snapshots = get_latest_rds_snapshot(env_prefix='playbackup')
-    dreambox.utils.print_structure(rds_snapshots)
-    print('--- testing get_latest_rds_snapshot')
