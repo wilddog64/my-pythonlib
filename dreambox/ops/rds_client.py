4787c38cd797b25ac81a1e3f39ae129d7a5d5cd6
Chengkai Liang <chengkai.liang@dreambox.com>
2016-09-06 08:54:45 -0700

    add get_g2_rds_snapshots function

--- dreambox/ops/rds_client.py
+++ dreambox/ops/rds_client.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from collections import defaultdict
 import dreambox.aws.rds as rds
 import dreambox.utils
 import dateutil.parser
@@ -43,15 +44,30 @@ get_sorted_rds_snapshots is a function return a list of ordered rds snapshots ba
 
     return [dict_ for (key, dict_) in decorated]
 
-def sort_g2_rds_snapshot_by_second_key(profile='', region='us-east-1', env_prefix='', sort_key='DBInstanceIdentifier'):
+def get_g2_rds_snapshots(profile='', region='us-east-1', env_prefix='production-g2'):
     '''
-sort_g2_rds_snapshot_by_second_key is a function that return a sorted list bases second key in the same list
-    '''
-    snapshopts = get_sorted_rds_snapshots(profile=profile, region=region, env_prefix=env_prefix)
-    decorated = [(dict_[sort_key][-1], dict_) for dict_ in snapshopts]
-    sorted_list = [dict_ for (key, dict_) in decorated]
+get_g2_rds_snapshots is a function that returns a hash of arrays whose key is a day stamp
 
-    return sorted_list
+* profile - an aws profile that provide credential information.  If this is
+	    None, it will first look at a default profile setting at
+	    ~/.aws/config. If it cannot find default profile, it will use node
+	    IAM profile if there's one exist for a given node.
+* regions - a list of regions to search for a target environment. If this is
+	    None, it will search for us-east-1 and us-west-2.
+* env_prefix - an environment prefix like stage1, ..., stage9
+    '''
+    snapshots = get_sorted_rds_snapshots(profile=profile, region=region, env_prefix=env_prefix)
+
+    # create a hash of arrays whose key is the time of the snapshots created
+    current_year = datetime.datetime.now().year
+    hash_table = {}
+    for snapshot in snapshots:
+        if str(current_year) in snapshot['DBSnapshotIdentifier']:
+            start_pos = snapshot['DBSnapshotIdentifier'].index(str(current_year))
+            key       = snapshot['DBSnapshotIdentifier'][start_pos:-6]
+            hash_table.setdefault(key, []).append(snapshot)
+
+    return hash_table
 
 if __name__ == '__main__':
     print('--- testing get_latest_rds_snapshot')
@@ -64,8 +80,8 @@ if __name__ == '__main__':
     dreambox.utils.print_structure(sorted_snapshots)
     print('--- testing get_sorted_rds_snaphots')
     print('')
-    print('--- testing sort_g2_rds_snapshot_by_second_key')
-    g2_rds_snapshots = sort_g2_rds_snapshot_by_second_key(region='us-east-1', env_prefix='production-g2')
-    dreambox.utils.print_structure(g2_rds_snapshots[0:5])
-    print('--- testing sort_g2_rds_snapshot_by_second_key')
+    print('--- testing get_g2_rds_snapshots')
+    g2_rds_snapshots = get_g2_rds_snapshots(region='us-east-1')
+    dreambox.utils.print_structure(g2_rds_snapshots)
+    print('--- testing get_g2_rds_snapshots')
 
