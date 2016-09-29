777f9c3e9663fab6ccee98e3e4fed9d4c245bf31
Chengkai Liang <chengkai.liang@dreambox.com>
2016-09-29 14:00:14 -0700

    add new parameter config_file_path to load_ini_config and config_section_map function

--- dreambox/config/core.py
+++ dreambox/config/core.py
@@ -3,19 +3,24 @@ import ConfigParser
 import os
 
 
-def load_ini_config(config_file=''):
+def load_ini_config(config_file='', config_file_path=''):
     '''
     load an ini configuration file from within a directory
     '''
-    script_dir          = os.path.dirname(os.path.realpath(__file__))
-    script_etc          = os.path.join(script_dir, '..', 'etc')
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    script_etc = ''
+    if config_file_path != '':
+        script_etc = os.path.expanduser(config_file_path)
+    else:
+        script_etc = os.path.join(script_dir, '..', 'etc')
     jenkins_config_path = os.path.join(script_etc, config_file)
     config              = ConfigParser.ConfigParser()
     config.read(jenkins_config_path)
 
     return config
 
-def config_section_map(config_file='', section_name=''):
+def config_section_map(config_file='', config_file_path='', section_name=''):
     '''
     provide an easy way to access option in an ini file section. This
     function accepts two parameters
@@ -30,7 +35,7 @@ def config_section_map(config_file='', section_name=''):
     given section
     '''
 
-    config = load_ini_config(config_file=config_file)
+    config = load_ini_config(config_file, config_file_path)
     section = {}
     for option in config.options(section_name):
         section[option] = config.get(section_name, option)
@@ -40,6 +45,8 @@ def config_section_map(config_file='', section_name=''):
 
 if __name__ == '__main__':
    print('--- testing config_section_map ---')
-   print('jenkins user %s: ' % config_section_map('jenkins.ini', 'jenkins')['user'])
-   print('jenkins url %s: ' %config_section_map('jenkins.ini', 'jenkins')['url'])
+   print('jenkins user %s: ' % config_section_map(config_file='jenkins.ini',
+                                                  section_name='stage-devops-jenkins')['user'])
+   print('jenkins url %s: ' %config_section_map(config_file='jenkins.ini',
+                                                section_name='stage-devops-jenkins')['url'])
    print('--- testing config_section_map ---')
